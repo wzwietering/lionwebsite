351b207dfccea5f8fed952f09ad1d548636b9d58
Wilmer Zwietering <wzwietering@gmail.com>
2017-03-01 17:08:04 +0100

    Styling of the map, map markers, updated map areas

--- js/maps.js
+++ js/maps.js
@@ -1,15 +1,30 @@
 var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 3,
-    center: {lat: 0, lng: 0}
-  });
+	map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 3,
+		center: {lat: 0, lng: 0}
+	});
   
-  map.data.loadGeoJson('../data/lionhabitat.geojson');
+	map.data.loadGeoJson('../data/lionhabitat.geojson');
   
-  map.data.setStyle({
-	  fillColor: '#E65100',
-	  strokeColor: '#B94000',
-	  strokeWeight: 1
+	//Style of the map
+	map.data.setStyle({
+		fillColor: '#E65100',
+		strokeColor: '#B94000',
+		strokeWeight: 1
+	});
+	
+	//Make lines bold on hover
+	map.data.addListener('mouseover', function(event) {
+	  map.data.revertStyle();
+	  map.data.overrideStyle(event.feature, {strokeWeight: 4});
+	});
+	
+	map.data.addListener('mouseout', function(event) {
+		map.data.revertStyle();
+	});
+	
+	map.data.addListener('click', function(event) {
+		
 	});
 }
