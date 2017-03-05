b513193b1b86fcf6e01ecd93ad137d6fd61f5dc4
Wilmer Zwietering <wzwietering@gmail.com>
2017-03-05 12:00:44 +0100

    Continued comment section

--- js/comments.js
+++ js/comments.js
@@ -1,19 +1,15 @@
 $(document).ready(function() {
-	var $visible = false;
 	var $loaded_comments = false;
 	
 	$("#comment_section").click(function() {
-		if(!$visible){
-			$visible = true;
+		if(!($("#comment_area").is(":visible"))){
 			if(!$loaded_comments){
 				$('#comment_area').load('../html/comment_section.html');
 				$loaded_comments = true;
-			} else {
-				$('#comment_area').show();
 			}
+			$('#comment_area').show();
 			$('#comment_section').prop('value', 'Hide comments');
 		} else {
-			$visible = false;
 			$('#comment_area').hide();
 			$('#comment_section').prop('value', 'Show comments');
 		}
