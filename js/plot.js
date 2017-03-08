92d36dd357b995c186699873cc049a23eb427b99
Jhsteensma <j.h.steensma@students.uu.nl>
2017-03-08 16:41:23 +0100

    Changes hotflot plot

--- js/plot.js
+++ js/plot.js
@@ -1,29 +1,30 @@
 $(document).ready(function()
 {
+		var label;
 		var options = {
 					xaxis: 
 						{ min:1950,max:2020}, 
 					yaxis:
 						{min:0, max: 200000},
 					series: {
-						lines: { show: true },
-						points: { show: true }
+						lines: { show: true},
+						points: { show: true },
+						color: ["#000"],
 						},
 					grid: {
 						hoverable: true,
 						clickable: true,
-						backgroundColor: { colors: ["#333", "#999"] }
+						backgroundColor: { colors: ["#66ff33", "#ff3333"] }
 					},
 					legend: {
 						position: "ne",
 						backgroundOpacity: 0.5,
-						noColumns: 0,
-						labelBoxBorderColor: "transparent"
 						}
 		};
 				
 		$.getJSON('https://raw.githubusercontent.com/wzwietering/lionwebsite/master/data/lionspopulation.json' , function(data){
         $.plot($("#placeholder") ,[data], options);
+		label = data.label;
 		});		
 
 		function showTooltip(x, y, contents) {
@@ -43,7 +44,11 @@ $(document).ready(function()
 				if (item) {
 					var x = item.datapoint[0];
 						y = item.datapoint[1];
-						str = "In " + x + " there were " + y + " " + label1 + "!";
+						if (y>50000){
+						str = "In " + x + " there were " + y + " " + label + "!";
+						} else {
+						str = "In " + x + " there were ONLY " + y + " " + label + "!";
+						}
 						showTooltip(item.pageX, item.pageY, str); 
 				} else {
 					$("#tooltip").remove();
