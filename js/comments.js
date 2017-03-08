59115323c75dfe3ff775d4be1364bbbd7ac9f8ad
Wilmer Zwietering <wzwietering@gmail.com>
2017-03-08 12:28:50 +0100

    Further work on the comment section

--- js/comments.js
+++ js/comments.js
@@ -1,17 +1,30 @@
 $(document).ready(function() {
-	var $loaded_comments = false;
-	
+	var $loaded = false;
 	$("#comment_section").click(function() {
 		if(!($("#comment_area").is(":visible"))){
-			if(!$loaded_comments){
-				$('#comment_area').load('../html/comment_section.html');
-				$loaded_comments = true;
+			if(!$loaded){
+				load();
 			}
-			$('#comment_area').show();
-			$('#comment_section').prop('value', 'Hide comments');
+			$("#comment_area").show();
+			$("#comment_section").prop("value", "Hide comments");
 		} else {
-			$('#comment_area').hide();
-			$('#comment_section').prop('value', 'Show comments');
+			$("#comment_area").hide();
+			$("#comment_section").prop("value", "Show comments");
 		}
 	});
+	
+	$("#submit").click(function() {
+		var $comment = $("#comment_input").val();
+		alert($comment);
+		$("#comment_input").val("");
+	});
+	
+	function load(){
+		$.get("/html/comment_section.html", function(response) {
+			for (var i = 0; i < 3; i++) {
+				$("#comment_area").prepend(response);
+			}
+		});
+		$loaded = true;
+	}
 });
