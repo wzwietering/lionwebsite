0d52d3a45167f264aa4ae5fbc98bb17b19616687
Wilmer Zwietering <wzwietering@gmail.com>
2017-03-08 16:06:22 +0100

    Comment section works!

--- js/comments.js
+++ js/comments.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 	var $loaded = false;
+	var $comments = [];
+	
 	$("#comment_section").click(function() {
 		if(!($("#comment_area").is(":visible"))){
 			if(!$loaded){
@@ -15,16 +17,42 @@ $(document).ready(function() {
 	
 	$("#submit").click(function() {
 		var $comment = $("#comment_input").val();
-		alert($comment);
+		var $username = $("#username_input").val();
+		createComment($username, $comment);
+		storeComment($username, $comment);
 		$("#comment_input").val("");
+		$("#username_input").val("");
 	});
 	
 	function load(){
-		$.get("/html/comment_section.html", function(response) {
-			for (var i = 0; i < 3; i++) {
-				$("#comment_area").prepend(response);
+		if (document.cookie.length > 0){
+			$comments.push(extractCookie(document.cookie));
+			var $data = extractCookie(document.cookie).split(",");
+			for (i = 0; i < $data.length; i+=2) {
+				createComment($data[i], $data[i+1]);
 			}
+			$loaded = true;
+		}
+	}
+	
+	function extractCookie($cookie){
+		return decodeURIComponent($cookie.split("=")[1]);
+	}
+	
+	function createComment($username, $comment) {
+		$.get("/html/comment_section.html", function(response) {
+			$("#comment_area").prepend(response);
+			$("#comment").text($comment);
+			$("#username").text("~" + $username);
 		});
-		$loaded = true;
+	}
+	
+	//Create cookie to store comment
+	function storeComment($username, $comment) {
+		$comments.push([$username, $comment]);
+		var $date = new Date();
+		$date.setTime($date.getTime() + (365*24*60*60*1000));
+		var $expires = "expires="+ $date.toUTCString();
+		document.cookie = "comments=" + encodeURIComponent($comments) + ";" + $expires + ";path=/";
 	}
 });
