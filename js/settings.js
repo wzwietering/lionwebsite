58c64bac5614926820e818cdd19d85fc5f2e78b6
Wilmer Zwietering <wzwietering@gmail.com>
2017-03-10 13:51:25 +0100

    Small changes to the settings

--- js/settings.js
+++ js/settings.js
@@ -18,7 +18,7 @@ function createCookie($name, $value){
 }
 
 function getCookie($name) {
-	var value = "; " + document.cookie;
-	var parts = value.split("; " + $name + "=");
-	if (parts.length == 2) return decodeURIComponent(parts.pop().split(";").shift());
+	var $value = "; " + document.cookie;
+	var $parts = $value.split("; " + $name + "=");
+	if ($parts.length == 2) return decodeURIComponent($parts.pop().split(";").shift());
 }
