1e3c7391cf8c72913c7f4661cb9ec76ff2e8a8fc
Wilmer Zwietering <wzwietering@gmail.com>
2017-03-15 14:21:38 +0100

    Custom colors

--- js/settings.js
+++ js/settings.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	$("#body").css("font-family", getCookie('font'));
+	changeStyle(getCookie('dark'), getCookie('darkest'), getCookie('light'));
 	
 	$("input[name=font]:radio").change(function () {
 		$font = $(this).val();
@@ -7,6 +8,22 @@ $(document).ready(function() {
 		
 		//Store selected font
 		createCookie('font', $font);
+	});
+	
+	$("input[name=color]:radio").change(function () {
+		$color = $(this).val();
+		var bodyStyles = window.getComputedStyle(document.body);
+		switch($color) {
+			case 'green':
+				changeStyle('#1DD600','#26BC70','#c1f9bd');
+				break;
+			case 'blue':
+				changeStyle('#1539B2','#0874A8','#ccccff');
+				break;
+			default:
+				changeStyle('#DB9200','#DB6300','#FFFDBC');
+				break;
+		}
 	});
 });
 	
@@ -21,4 +38,13 @@ function getCookie($name) {
 	var $value = "; " + document.cookie;
 	var $parts = $value.split("; " + $name + "=");
 	if ($parts.length == 2) return decodeURIComponent($parts.pop().split(";").shift());
+}
+
+function changeStyle($dark, $darkest, $light){
+	document.body.style.setProperty('--dark', $dark);
+	document.body.style.setProperty('--darkest', $darkest);
+	document.body.style.setProperty('--light', $light);
+	createCookie('dark', $dark);
+	createCookie('darkest', $darkest);
+	createCookie('light', $light);
 }
