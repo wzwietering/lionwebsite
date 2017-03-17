78e73ec05b6db7e023e9e98629c9bf2262785642
Wilmer Zwietering <wzwietering@gmail.com>
2017-03-17 15:02:19 +0100

    Marker clustering

--- js/maps.js
+++ js/maps.js
@@ -2,21 +2,12 @@ $(document).ready(function() {
 	var map;
 	var $defaultZoom = 3;
 	var $defaultCenter = {lat: 20, lng: 10};
+	var markers = []
 	
 	$(function initMap() {
 		map = new google.maps.Map(document.getElementById('map'), {
 			zoom: $defaultZoom,
 			center: $defaultCenter
-		});
-	  
-		map.data.loadGeoJson('https://raw.githubusercontent.com/wzwietering/lionwebsite/master/data/lionhabitat.geojson');
-		var data = map.data;
-		data.forEach(function(feature){
-			alert('hallo');
-		});
-		
-		$.each(data.features, function(key, val) {
-			alert('hi');
 		});
 	  
 		//Style of the map
@@ -38,18 +29,36 @@ $(document).ready(function() {
 		
 		map.data.addListener('click', function(event) {
 			map.setCenter(event.latLng);
-			//Description and zoom is only for the point
-			if(event.feature.getGeometry().getType() == "Point"){
-				map.setZoom(12);
-				var infowindow = new google.maps.InfoWindow({
-					content: event.feature.getProperty("Name"),
-					position: event.latLng
-				});
-				infowindow.open(map);
-			} else if(event.feature.getGeometry().getType() == "Polygon"){
+			if(event.feature.getGeometry().getType() == "Polygon"){
 				map.setZoom(5);
 			}
 		});
+	});
+	
+	function calculate(map) {
+		map.data.forEach(function (feature) {
+			if(feature.getGeometry().getType() == "Point"){;
+				var marker = new google.maps.Marker({
+					position: feature.getGeometry().get(),
+					title: feature.getProperty('name'),
+					map: map
+				});				
+				markers.push(marker);
+				//Remove point, because we created a marker in it's place
+				map.data.remove(feature);
+			}
+		});
+		var markerCluster = new MarkerClusterer(map, markers, {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+	}
+
+	function loadGeoJson(map) {
+		map.data.loadGeoJson('https://raw.githubusercontent.com/wzwietering/lionwebsite/master/data/lionhabitat.geojson');
+	}
+
+	google.maps.event.addDomListener(window, 'load', function () {
+		$('initMap');
+		loadGeoJson(map);
+		setTimeout(function() {calculate(map);}, 500);
 	});
 
 
